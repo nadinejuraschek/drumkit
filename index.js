3fe49982b607933a3b6c2f14cc956dfed559a0ad
Nadine Ju <nadine.jura@online.de>
2020-03-30 21:51:43 -0700

    different sounds for each button

--- index.js
+++ index.js
@@ -3,11 +3,62 @@ const drumBtns = document.querySelectorAll('.drum').length;
 for (let i=0; i < drumBtns; i++) {
     
     document.querySelectorAll('.drum')[i].addEventListener('click', function() {
-        
-        let sound = new Audio('sounds/tom-1.mp3');
 
-        sound.play();
+        let btnInnerHTML = this.innerHTML;
 
+        switch (btnInnerHTML) {
+            case 'w':
+
+                let tomOne = new Audio('sounds/tom-1.mp3');
+                tomOne.play();
+
+                break;
+            
+            case 'a':
+                
+                let tomTwo = new Audio('sounds/tom-2.mp3');
+                tomTwo.play();
+
+                break;
+
+            case 's':
+                
+                let tomThree = new Audio('sounds/tom-3.mp3');
+                tomThree.play();
+                    
+                break;
+
+            case 'd':
+                
+                let tomFour = new Audio('sounds/tom-4.mp3');
+                tomFour.play();
+                        
+                break;
+                
+            case 'j':
+                
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                            
+                break;
+
+            case 'k':
+                
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                                
+                break;
+
+            case 'l':
+                
+                let kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                                
+                break;
+            
+            default: console.log('Clicked');
+
+        };
     });
 
 };
